e34984909f00cbf83e698f9b4b230bc62b78bbef
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-07-17 23:41:04 +0600

    feat: Update database connection handling to utilize configuration from environment variables

--- os-go-server-ecommerce/config/config.go
+++ os-go-server-ecommerce/config/config.go
@@ -11,11 +11,21 @@ import (
 
 var configurations *Config
 
+type DBConfig struct {
+	Host          string
+	Port          int
+	Name          string
+	User          string
+	Password      string
+	EnableSSlMode bool
+}
+
 type Config struct {
 	Version      string
 	ServiceName  string
 	HttpPort     int
 	JwtSecretKey string
+	DB           *DBConfig
 }
 
 func loadConfig() {
@@ -55,11 +65,64 @@ func loadConfig() {
 		os.Exit(1)
 	}
 
+	dbHost := os.Getenv("HOST")
+	if dbHost == "" {
+		fmt.Println("DB Host is required")
+		os.Exit(1)
+	}
+
+	dbPort := os.Getenv("PORT")
+	if dbPort == "" {
+		fmt.Println("DB port is required")
+		os.Exit(1)
+	}
+	DbPort, err := strconv.ParseInt(dbPort, 10, 64)
+	if err != nil {
+		fmt.Println("Port must be number")
+		os.Exit(1)
+	}
+
+	dbName := os.Getenv("NAME")
+	if dbName == "" {
+		fmt.Println("DB Name is required")
+		os.Exit(1)
+	}
+
+	dbUser := os.Getenv("USER")
+	if dbUser == "" {
+		fmt.Println("DB User is required")
+		os.Exit(1)
+	}
+
+	dbPassword := os.Getenv("PASSWORD")
+	if dbPassword == "" {
+		fmt.Println("DB Password is required")
+		os.Exit(1)
+	}
+
+	enableSSLMode := os.Getenv("ENABLE_SSL_MODE")
+
+	parsedEnableSSLMode, err := strconv.ParseBool(enableSSLMode)
+	if err != nil {
+		fmt.Println("Invalid Enable SSL MODE value", err)
+		os.Exit(1)
+	}
+
+	dbConfig := &DBConfig{
+		Host: dbHost,
+		Port: int(DbPort),
+		Name: dbName,
+		User: dbUser,
+		Password: dbPassword,
+		EnableSSlMode: parsedEnableSSLMode,
+	}
+
 	configurations = &Config{
-		Version:     VERSION,
-		ServiceName: SERVICE_NAME,
-		HttpPort:    int(port), // typecasting
+		Version:      VERSION,
+		ServiceName:  SERVICE_NAME,
+		HttpPort:     int(port), // typecasting
 		JwtSecretKey: jwtSecretKey,
+		DB: dbConfig,
 	}
 }
 
